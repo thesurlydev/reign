666d03867b535379c014b0e440c140dd8080f207
shane <shane@digitalsanctum.com>
2022-01-29 15:05:21 -0800

    end to end working

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,21 @@
 extern crate clap;
 extern crate core;
+extern crate port_scanner;
+
+use port_scanner::scan_port_addr;
 
 use std::thread::sleep;
 use std::time::Duration;
 
 use aws_config::meta::region::RegionProviderChain;
 use aws_config::RetryConfig;
-use aws_sdk_ec2::Client;
+use aws_sdk_ec2::{Client, Region};
 use aws_sdk_ec2::Error;
-use aws_sdk_ec2::model::{Filter, IamInstanceProfileSpecification, Instance,
-                         InstanceNetworkInterfaceSpecification, InstanceType, Reservation,
-                         ResourceType, Tag, TagSpecification};
+use aws_sdk_ec2::model::{Filter, IamInstanceProfileSpecification, Instance, InstanceNetworkInterfaceSpecification, InstanceType, Reservation, ResourceType, Tag, TagSpecification};
 use aws_sdk_ec2::output::DescribeInstancesOutput;
 use clap::{AppSettings, Parser};
-use serde_derive::{Deserialize, Serialize};
 
-#[derive(Parser, Debug, Serialize, Deserialize)]
+#[derive(Parser, Debug, PartialEq, Clone, Ord, PartialOrd, Eq)]
 #[clap(version, setting = AppSettings::ArgRequiredElseHelp)]
 struct Config {
     /// Name to tag instance with
@@ -25,90 +25,85 @@ struct Config {
     #[clap(short, long)]
     file: Option<String>,
     /// Instance type of the VM
-    #[clap(short, long)]
+    #[clap(short, long, default_value = "t3.nano")]
     instance_type: String,
-    /// Linux distribution
-    #[clap(short, long)]
-    distro: String,
-    /// AMI to use for the VM
-    #[clap(short, long)]
+    /// Amazon Machine Image
+    #[clap(short, long, default_value = "ami-036d46416a34a611c")]
     ami: String,
     /// SSH user
-    #[clap(short, long)]
-    user: Option<String>,
+    #[clap(short, long, default_value = "ubuntu")]
+    user: String,
     /// ID of the subnet to run the VM on
-    #[clap(short, long)]
+    #[clap(short, long, default_value = "subnet-89ef61d3")]
     subnet: String,
     /// Security group IDs
-    #[clap(short, long)]
+    #[clap(short, long, default_values = &["sg-37d22f44"])]
     groups: Vec<String>,
-    /// Name of the IAM instance profile
-    #[clap(short, long)]
-    role: String,
+    /// Name of the IAM role
+    #[clap(long, default_value = "digitalsanctum-role")]
+    iam_role: String,
+    /// Name of the AWS region
+    #[clap(short, long, default_value = "us-west-2", env = "AWS_REGION")]
+    region: String,
     /// AWS profile
-    #[clap(short, long)]
+    #[clap(short, long, env = "AWS_PROFILE")]
     profile: Option<String>,
+    /// Key name
+    #[clap(short, long, default_value = "beefcake")]
+    key: String,
 }
-/*
-impl Default for Config {
-    fn default() -> Self {
-        Config {
-            name: String::new(),
-            instance_type: String::new(),
-            distro: String::new(),
-            ami: String::new(),
-            ssh_user: String::new(),
-            subnet: String::new(),
-            security_group: String::new(),
-            iam_role: String::from("digitalsanctum-role"),
-            aws_account_id: String::new(),
-            aws_profile: String::new(),
-        }
-    }
-}*/
-
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     let config: Config = Config::parse();
-    println!("{:?}", config);
 
-    let region_provider = RegionProviderChain::default_provider().or_else("us-west-2");
+    let conf_region: Region = Region::new(config.to_owned().region);
+
+    let region_provider = RegionProviderChain::default_provider().or_else(conf_region);
     let retry_config: RetryConfig = RetryConfig::default().with_max_attempts(5);
     let shared_config = aws_config::from_env().region(region_provider).retry_config(retry_config).load().await;
     let client = Client::new(&shared_config);
 
-    let instance_id = ec2_run_instance(&client).await?;
+    // run EC2 instance
+    let instance_id = ec2_run_instance(&client, &config).await?;
+    println!("InstanceId: {instance_id}");
     sleep(Duration::from_secs(2));
-    let instance = ec2_wait_for_state(&client, instance_id, "running").await?;
 
-    println!("{:?}", instance);
+    // wait for running state
+    let instance = ec2_wait_for_state(&client, instance_id, "running").await?;
+    println!("Instance is running");
 
     // get public DNS
-    /*if let Some(instance) = instance {
-        let mut public_dns = String::default();
-        for interface in instance.network_interfaces().unwrap_or_default() {
-            if let Some(pub_dns) = interface.association().unwrap().public_dns_name() {
-                public_dns = pub_dns.to_string();
-            }
+    let mut public_dns = String::default();
+    for interface in instance.network_interfaces().unwrap_or_default() {
+        if let Some(pub_dns) = interface.association().unwrap().public_dns_name() {
+            public_dns = pub_dns.to_string();
         }
-        println!("Public DNS: {public_dns}");
-    }*/
+    }
+    println!("Public DNS: {public_dns}");
+
+    // wait for SSH port to become available
+    let address = format!("{public_dns}:22");
+    wait_for_open_port(&address).await;
+    println!("SSH port is now available");
+    println!();
+    println!("Connection string: ssh -i ~/.ssh/beefcake.pem {}@{public_dns}", config.user);
+    println!();
 
     Ok(())
 }
 
-async fn ec2_run_instance(client: &Client) -> Result<String, Error> {
+async fn ec2_run_instance(client: &Client, config: &Config) -> Result<String, Error> {
     let network_spec = InstanceNetworkInterfaceSpecification::builder()
         .associate_public_ip_address(true)
         .device_index(0)
-        .subnet_id("subnet-89ef61d3")
+        .subnet_id(&config.subnet)
         .groups("sg-37d22f44")
         .build();
 
     let tag_spec = TagSpecification::builder()
         .resource_type(ResourceType::Instance)
-        .tags(Tag::builder().key("Name").value("rust-test").build())
+        .tags(Tag::builder().key("Name").value(&config.name).build())
         .build();
 
     let iam_spec = IamInstanceProfileSpecification::builder()
@@ -143,9 +138,17 @@ async fn ec2_run_instance(client: &Client) -> Result<String, Error> {
         panic!("InstanceId was not returned");
     };
 
-    println!("InstanceId: {id}");
-
     Ok(id.to_string())
+}
+
+async fn wait_for_open_port(address: &String) {
+    loop {
+        if scan_port_addr(address) {
+            break;
+        } else {
+            sleep(Duration::from_millis(500))
+        }
+    }
 }
 
 async fn ec2_wait_for_state(client: &Client, id: String, state: &str) -> Result<Instance, Error> {
